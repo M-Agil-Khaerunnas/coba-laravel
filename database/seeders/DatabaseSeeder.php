078e653400114f0e7fe438cda9b8558b502e8385
rill05 <masnuri945@gmail.com>
2023-03-31 09:32:21 +0700

    video 22

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,11 +17,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         
-        // User::create([
-            //     'name' => 'Agiell',
-            //     'email' => 'Khaerunnasagil@gmail.com',
-            //     'password' => bcrypt('12345')
-            // ]); 
+        User::create([
+                'name' => 'Agiell',
+                'username' => 'Agiels',
+                'email' => 'Khaerunnasagil@gmail.com',
+                'password' => bcrypt('12345')
+            ]); 
             
             // User::create([
                 //     'name' => 'Primiily',
